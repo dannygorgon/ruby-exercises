695500011ad3465ec6715ca3389d293a1354d095
dannygorgon <thirdoptionisthename@googlemail.com>
2024-03-21 11:53:09 +0000

    implement method to remove all but last element from array

--- ruby_basics/6_arrays/exercises/array_exercises.rb
+++ ruby_basics/6_arrays/exercises/array_exercises.rb
@@ -27,7 +27,7 @@ def remove_last_element(array)
   # Step 1: remove the last element from the array
 
   # Step 2: return the array (because Step 1 returns the value of the element removed)
-  array
+  array.length < 3 ? [] : array[0..-2]
 end
 
 def remove_first_three_elements(array)
