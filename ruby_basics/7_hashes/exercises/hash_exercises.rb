519ba5f2544feb53ddf670ed8a13b7057160bb98
dannygorgon <thirdoptionisthename@googlemail.com>
2024-03-22 17:48:40 +0000

    implement method to return the favorite color and number

--- ruby_basics/7_hashes/exercises/hash_exercises.rb
+++ ruby_basics/7_hashes/exercises/hash_exercises.rb
@@ -1,5 +1,6 @@
 def create_favorite_hash(color, number)
   # return a hash with the following key/value pairs:
+  {color: color, number: number}
   # key of color (as a symbol) with value of the color argument
   # key of number (as a symbol) with the value of the number argument
 end
